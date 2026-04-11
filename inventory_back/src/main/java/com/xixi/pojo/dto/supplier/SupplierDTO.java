fea58cb2135ebf298dce2970015fefce8f5c83e7
XiXiHaHa <2685524150@qq.com>
2026-04-11 19:43:48 +0800

    feat:add supplier dto and page query

---
+++ inventory_back/src/main/java/com/xixi/pojo/dto/supplier/SupplierDTO.java
@@ -0,0 +1,14 @@
+package com.xixi.pojo.dto.supplier;
+
+public class SupplierDTO {
+    private Long id;
+    private Long userId;
+    private  String code;
+    private  String name;
+    private  String contactName;
+    private  String contactPhone;
+    private  String email;
+    private  String address;
+    private  String licenseNo;
+    private  String remark;
+}
---
+++ inventory_back/src/main/java/com/xixi/pojo/query/supplier/SupplierPageQuery.java
@@ -0,0 +1,9 @@
+package com.xixi.pojo.query.supplier;
+
+public class SupplierPageQuery {
+    private String code;
+    private  String name;
+    private  String contactName;
+    private  String contactPhone;
+    private String status;
+}
